cd5529337974f3be7b899e9dec4e127758f632ee
RasLen <sabirovrasul@gmail.com>
2023-04-01 18:59:13 +0600

    sig page upd 1

--- FInal/app/src/main/java/com/example/afinal/MainActivity.kt
+++ FInal/app/src/main/java/com/example/afinal/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.afinal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var button : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.sign_in_button)
+
+        button.setOnClickListener {
+            val intent = Intent(this@MainActivity, activity_sign_in::class.java)
+            startActivity(intent)
+        }
     }
+
 }
