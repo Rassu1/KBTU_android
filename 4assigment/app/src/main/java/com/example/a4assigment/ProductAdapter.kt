e55ec26eb52e8ee12915b0184232299b7e1d3852
RasLen <sabirovrasul@gmail.com>
2023-05-12 11:12:58 +0600

    last

---
+++ 4assigment/app/src/main/java/com/example/a4assigment/ProductAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.a4assigment
+
+class ProductAdapter {
+}
---
+++ 4assigment/app/src/main/java/com/example/a4assigment/ServiceInterface.kt
@@ -0,0 +1,12 @@
+package com.example.a4assigment
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Headers
+
+interface ServiceInterface {
+
+    @Headers("Content-Type:application/json")
+    @GET("/products")
+    fun getAllProducts():Call<ApiResponse>
+}
